8669c3fe882f2b6bd785a3836770b45907fc8e82
Matthew Gammon <mgammon91@gmail.com>
2021-07-26 17:19:34 -0400

    chained on all(methodNotAllowed) to the end of the '/' route.

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controller = require('./movies.controller');
 const methodNotAllowed = require('../errors/methodNotAllowed');
 
-router.route('/').get(controller.list);
+router.route('/').get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
