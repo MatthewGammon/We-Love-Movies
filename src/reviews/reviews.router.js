1ea26ebc94dffe5e7ea58bffb83c21186934c5ef
Matthew Gammon <mgammon91@gmail.com>
2021-07-27 18:54:10 -0400

    built endpoint to /reviews/:reviewId

---
+++ src/reviews/reviews.router.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const controller = require('./reviews.controller');
+const methodNotAllowed = require('../errors/methodNotAllowed');
+router.route('/:reviewId').delete(controller.destroy).all(methodNotAllowed);
+
+module.exports = router;
