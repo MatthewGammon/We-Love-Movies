48485d2288ee62d5ba626543633980c4e50c7be5
Matthew Gammon <mgammon91@gmail.com>
2021-07-27 00:15:25 -0400

    created middleware function to check if the movieId entered in route params exists. created async 'read' function that retuns the movie object if it exists

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,6 +1,23 @@
 const service = require('./movies.service');
 const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
+async function movieExists(req, res, next) {
+  const movieId = Number(req.params.movieId);
+  const foundMovie = await service.read(movieId);
+  if (foundMovie) {
+    res.locals.movie = foundMovie;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Movie with id ${movieId} does not exist!`,
+  });
+}
+
+async function read(req, res, next) {
+  res.json({ data: res.locals.movie });
+}
+
 async function list(req, res, next) {
   const isShowing = req.query.is_showing;
   if (isShowing) {
@@ -11,4 +28,5 @@ async function list(req, res, next) {
 
 module.exports = {
   list: asyncErrorBoundary(list),
+  read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
 };
