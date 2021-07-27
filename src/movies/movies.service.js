446792ab40c91f95816b6a9f64994d17fa80878a
Matthew Gammon <mgammon91@gmail.com>
2021-07-27 15:44:27 -0400

    removed old function that is now taken care of with the modify method inside 'list'

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -28,28 +28,6 @@ function list(isShowing) {
     });
 }
 
-function activeMovies() {
-  return knex('movies_theaters as mt')
-    .join('movies as m', 'mt.movie_id', 'm.movie_id')
-    .select(
-      'm.movie_id as id',
-      'm.title',
-      'm.runtime_in_minutes',
-      'm.rating',
-      'm.description',
-      'm.image_url'
-    )
-    .where({ 'mt.is_showing': true })
-    .groupBy(
-      'm.movie_id',
-      'm.title',
-      'm.runtime_in_minutes',
-      'm.rating',
-      'm.description',
-      'm.image_url'
-    );
-}
-
 function theatersByMovie(movieId) {
   return knex('theaters')
     .select(
