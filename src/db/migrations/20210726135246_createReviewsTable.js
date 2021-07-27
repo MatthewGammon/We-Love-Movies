953f4d30dc32c0e51c24c47cea15b892b8512c36
Matthew Gammon <mgammon91@gmail.com>
2021-07-27 15:02:16 -0400

    reverted a reversion that had removed my current list function

--- src/db/migrations/20210726135246_createReviewsTable.js
+++ src/db/migrations/20210726135246_createReviewsTable.js
@@ -15,10 +15,6 @@ exports.up = function (knex) {
       .references('movie_id')
       .inTable('movies')
       .onDelete('cascade');
-
-    table.timestamps(true, true);
-    // adds created_at and updated_at columns; passing true as the first argument sets the columns to be a timestamp type while passing true as
-    // the second argument sets those columns to be non-nullable and to use the current timestamp by default
   });
 };
 
--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,10 +1,8 @@
-const router = require('express').Router({ mergeParams: true });
+const router = require('express').Router();
 const controller = require('./movies.controller');
 const methodNotAllowed = require('../errors/methodNotAllowed');
 
 router.route('/').get(controller.list).all(methodNotAllowed);
-router.route('/:movieId/theaters').get(controller.theatersList);
-router.route('/:movieId/reviews').get(controller.reviewsList);
 router.route('/:movieId').get(controller.read);
 
 module.exports = router;
