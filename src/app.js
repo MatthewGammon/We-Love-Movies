1cd798ce9a887a80d854dfae04aebd3c9853bf22
Matthew Gammon <mgammon91@gmail.com>
2021-07-27 18:53:15 -0400

    added /reviews endpoint, handled by reviewsRouter

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const errorHandler = require('./errors/errorHandler');
 const notFound = require('./errors/notFound');
 const moviesRouter = require('./movies/movies.router');
+const reviewsRouter = require('./reviews/reviews.router');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/movies', moviesRouter);
+app.use('/reviews', reviewsRouter);
 app.use(notFound);
 app.use(errorHandler);
 
