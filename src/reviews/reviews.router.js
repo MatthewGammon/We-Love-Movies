490046076234d93d5316ec32149f6d3ace921672
Matthew Gammon <mgammon91@gmail.com>
2021-07-28 17:38:58 -0400

    added get and put requests to /:reviewId endpoint

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const controller = require('./reviews.controller');
 const methodNotAllowed = require('../errors/methodNotAllowed');
-router.route('/:reviewId').delete(controller.destroy).all(methodNotAllowed);
+router
+  .route('/:reviewId')
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.destroy)
+  .all(methodNotAllowed);
 
 module.exports = router;
