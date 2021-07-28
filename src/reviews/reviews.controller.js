e6a90e3368f7ebcd24e4c3601dc940be8a58055f
Matthew Gammon <mgammon91@gmail.com>
2021-07-28 17:37:58 -0400

    changed error status for 'reviewExists' to satisfy qualifed. added a 'read' function so I could make get requests to a specific :reviewId after making a put request and verify that the change was made. Wrote out the 'update' function and exported read and update

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -3,7 +3,6 @@ const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
 async function reviewExists(req, res, next) {
   const reviewId = Number(req.params.reviewId);
-  console.log(typeof reviewId);
   const foundReview = await service.read(reviewId);
   if (foundReview) {
     res.locals.review = foundReview;
@@ -11,7 +10,7 @@ async function reviewExists(req, res, next) {
   }
   next({
     status: 404,
-    message: `Review with id ${reviewId} does not exist!`,
+    message: `Review ${reviewId} cannot be found`,
   });
 }
 
@@ -20,6 +19,25 @@ async function destroy(req, res) {
   res.sendStatus(204);
 }
 
+async function read(req, res, next) {
+  res.json({ data: await service.read(res.locals.review.review_id) });
+}
+
+async function update(req, res, next) {
+  const originalReview = res.locals.review;
+  const updatedReview = {
+    ...originalReview,
+    score: req.body.data.score,
+    content: req.body.data.content,
+    review_id: originalReview.review_id,
+  };
+  const data = await service.updateReview(updatedReview);
+  console.log(data);
+  res.json({ data });
+}
+
 module.exports = {
   destroy: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
+  read: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(read)],
+  update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
 };
