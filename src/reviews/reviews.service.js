11b6a169b544eebbdad1176815d0c8fdea15df96
Matthew Gammon <mgammon91@gmail.com>
2021-07-29 12:39:46 -0400

    removed comment from troubleshooting

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -27,7 +27,6 @@ function updateReview(updatedReview) {
 
 function destroy(reviewId) {
   return knex('reviews').where({ review_id: reviewId }).first().del();
-  //   Received: "select * from `reviews` where `reviewId` = '337' - SQLITE_ERROR: no such column: reviewId
 }
 
 module.exports = {
